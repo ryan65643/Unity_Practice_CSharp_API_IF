cca6eee01b5f3530546373776e1002b71806c821
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2020-07-17 23:40:11 +0800

    Work_5

---
+++ CSharp_API_IF/Assets/KID/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("速度"), Range(0, 1000)]
+    public int speed = 4;
+    public Animator Ani;
+    public Rigidbody rig;
+
+    private void Move()
+    {
+        bool key = Input.GetKey(KeyCode.W);
+        Ani.SetBool("走路開關",key);
+        if (key)
+        {
+            rig.AddForce(new Vector3(0,0,speed));
+        }
+    }
+    private void Start()
+    {
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+}
